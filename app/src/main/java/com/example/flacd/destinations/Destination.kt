8fa5343584789ab0a34b012db29acf77cffe9500
obafisoye <dannieoba@gmail.com>
2024-10-21 16:23:35 -0500

    Added Screens and Updated Destinations

---
+++ app/src/main/java/com/example/flacd/destinations/Destination.kt
@@ -0,0 +1,14 @@
+package com.example.flacd.destinations
+
+/*
+    This class is responsible for where the app should navigate to.
+    by using sealed class, we are not able to further subclass this class.
+ */
+
+sealed class Destination(val route: String) {
+    object Home: Destination("home")
+    object Search: Destination("search")
+    object Profile: Destination("profile")
+    object AlbumDetail: Destination("albumDetail/{albumId}")
+        fun createRoute(albumId: Int?) = "albumDetail/$albumId"
+}
--- app/src/main/java/com/example/flacd/screens/AlbumDetailScreen.kt
+++ app/src/main/java/com/example/flacd/screens/AlbumDetailScreen.kt
@@ -1,2 +1,20 @@
 package com.example.flacd.screens
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+
+@Composable
+fun AlbumDetailScreen(modifier: Modifier = Modifier){
+    Box(
+        modifier = modifier
+            .fillMaxSize()
+            .background(Color.DarkGray)
+    ){
+        Text(text = "Album Detail Screen", color = Color.White)
+    }
+}
