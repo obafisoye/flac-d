ec5dde7f33aaa1f6248adbbae971153e40025d4b
obafisoye <dannieoba@gmail.com>
2024-10-22 20:22:25 -0500

    Initial Commit

---
+++ app/src/main/java/com/example/flacd/screens/ProfileDetailScreen.kt
@@ -0,0 +1,20 @@
+package com.example.flacd.screens
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+
+@Composable
+fun ProfileDetailScreen(modifier: Modifier = Modifier){
+    Box(
+        modifier = modifier
+            .fillMaxSize()
+            .background(Color.DarkGray)
+    ){
+        Text(text = "Profile Detail Screen", color = Color.White)
+    }
+}
